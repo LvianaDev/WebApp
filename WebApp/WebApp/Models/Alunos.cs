4fcf69ffb71659d3d0576ff112a63ac48e4e67fe
Lucas Viana <lucasg.viana@outlook.com>
2021-11-04 01:49:18 -0300

    Iniciando Projeto

---
+++ WebApp/WebApp/Models/Alunos.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace WebApp.Models
+{
+    public class Alunos
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string Telefone { get; set; }
+        public int Ra { get; set; }
+
+        public List<Alunos> listaAlunos()
+        {
+            Alunos aluno = new Alunos();
+            aluno.Id = 1;
+            aluno.Nome = "Lucas";
+            aluno.Sobrenome = "Viana";
+            aluno.Telefone = "11961345881";
+            aluno.Ra = 00001;
+
+            List<Alunos> listaAlunos = new List<Alunos>();
+
+            listaAlunos.Add(aluno);
+
+            return listaAlunos;
+        }
+    }
+}
